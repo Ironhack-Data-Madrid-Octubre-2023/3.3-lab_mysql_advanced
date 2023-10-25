9be8f7cff7c04ace67e44aca978336396a3c9af8
Jacobo Mate <mateblanquerjacobo@gmail.com>
2023-10-25 19:55:05 +0200

    Challenge 1: Steps 1, 2 y 3 hechos

--- your-code/solutions.sql
+++ your-code/solutions.sql
@@ -32,7 +32,22 @@ FROM
     ORDER BY TotalRoyalty DESC;
 
 -- Step 3
-
+SELECT 
+    Author_ID,
+    SUM(subquery.Royalty + t.advance) AS TotalRoyaltyAdvance
+FROM
+    (SELECT 
+        t.title_id Title_ID,
+            ta.au_id Author_ID,
+            (t.price * s.qty * t.royalty / 100 * ta.royaltyper / 100) Royalty
+    FROM
+        titles t
+    INNER JOIN titleauthor ta ON t.title_id = ta.title_id
+    INNER JOIN sales s ON t.title_id = s.title_id) AS subquery
+        LEFT JOIN
+    titles t ON subquery.Title_ID = t.title_id
+GROUP BY subquery.Author_ID
+ORDER BY TotalRoyaltyAdvance DESC;
        
        
        
