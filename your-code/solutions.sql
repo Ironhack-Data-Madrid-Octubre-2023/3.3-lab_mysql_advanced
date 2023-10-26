5fdea03576f67ad35195473fb3dfd1ccf6df1f29
LeoBulcsu <136447924+LeoBulcsu@users.noreply.github.com>
2023-10-26 12:33:45 +0200

    Update solutions_TERMINADO.sql

--- your-code/solutions.sql
+++ your-code/solutions.sql
@@ -29,5 +29,51 @@ GROUP BY tr.au_id;
 
 -- Challenge 2 - Alternative Solution 
 
+USE publications;
+
+CREATE TEMPORARY TABLE publications.royalties
+
+SELECT  t.title_id,
+		ta.au_id,
+		t.advance,
+		T.price * s.qty * t.royalty / 100 * ta.royaltyper / 100 AS 'ROYALTY'
+
+FROM titles AS t 
+INNER JOIN titleauthor AS ta
+ON t.title_id = ta.title_id
+INNER JOIN sales AS s
+ON t.title_id = s.title_id;
+
+CREATE TEMPORARY TABLE publications.royaltiessum
+
+SELECT  royalties.title_id,
+		royalties.au_id,
+        royalties.advance,
+		SUM(ROYALTY) royalty
+
+FROM publications.royalties
+
+GROUP BY royalties.title_id, royalties.au_id, royalties.advance;
+
+SELECT royaltiessum.au_id as 'Author ID',
+	   sum(royalty + royaltiessum.advance) as 'TOTAL EARNINGS'
+
+FROM publications.royaltiessum
+
+GROUP BY royaltiessum.au_id;
+
 -- Challenge 3
 
+USE publications;
+
+CREATE TABLE most_profiting_authors
+
+SELECT royaltiessum.au_id as 'Author ID',
+	   sum(royalty + royaltiessum.advance) as 'TOTAL EARNINGS'
+
+FROM publications.royaltiessum
+
+GROUP BY royaltiessum.au_id;
+
+SELECT * 
+FROM most_profiting_authors;
