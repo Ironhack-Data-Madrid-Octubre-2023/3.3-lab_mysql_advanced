f297f9d11301923e33e32a4b0457ba94af8cfa1d
Cristiane Carneiro <mccristiane@gmail.com>
2023-10-26 17:33:06 +0200

    progress

--- your-code/solutions.sql
+++ your-code/solutions.sql
@@ -1,2 +1,42 @@
--- Challenge 1 - Most Profiting Authors
+-- CHALLENGE 1 - Most Profiting Authors
+
+-- Step 1
+
+CREATE TEMPORARY TABLE royaltiespersale
+SELECT 
+    a.au_id,
+    t.title_id,
+    (t.price * s.qty * t.royalty / 100 * ta.royaltyper / 100) AS sales_royalty
+FROM
+    sales AS s
+        INNER JOIN
+    titles AS t ON s.title_id = t.title_id
+        INNER JOIN
+    titleauthor AS ta ON t.title_id = ta.title_id
+        INNER JOIN
+    authors AS a ON ta.au_id = a.au_id;
+    
+    
+-- Step 2 
+
+CREATE TEMPORARY TABLE step22
+SELECT 
+    au_id, title_id, sum(sales_royalty) AS aggroyalty
+FROM
+    step1
+GROUP BY 
+	au_id, title_id;
+    
+
+-- Step 3
+
+SELECT 
+    a.au_id, ROUND(SUM(advance + aggroyalty), 1) AS totalprofit
+FROM
+    step22
+        INNER JOIN
+    titles AS t ON step22.title_id = t.title_id
+        INNER JOIN
+    authors AS a ON step22.au_id = a.au_id
+GROUP BY a.au_id;
 
