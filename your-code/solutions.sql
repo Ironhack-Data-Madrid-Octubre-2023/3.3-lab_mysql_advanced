ac085d47c5e8145c98e32314c4651611ed06e1a6
LeoBulcsu <136447924+LeoBulcsu@users.noreply.github.com>
2023-10-26 11:44:12 +0200

    Update solutions_2.sql

--- your-code/solutions.sql
+++ your-code/solutions.sql
@@ -1,17 +1,33 @@
-USE publications;
+-- Challenge 1 - Most Profiting Authors
 
-SELECT  `TITLE ID`,
-	    `AUTHOR ID`,
-        SUM(ROYALTY)
+use publications;
+
+SELECT tr.au_id as 'Author ID',
+	   sum(royalty + tr.advance) as 'TOTAL EARNINGS'
 FROM
 
-(SELECT  T.TITLE_ID AS `TITLE ID`,
-	    TA.AU_ID AS `AUTHOR ID`,
-        t.price * s.qty * t.royalty / 100 * ta.royaltyper / 100 AS 'ROYALTY'
+(SELECT royalties.title_id,
+		royalties.au_id,
+        royalties.advance,
+		SUM(ROYALTY) royalty
+FROM
 
-FROM TITLES AS T 
-INNER JOIN TITLEAUTHOR AS TA
-ON T.TITLE_ID = TA.TITLE_ID
-INNER JOIN SALES AS S
-ON T.TITLE_ID = S.TITLE_ID) AS ROYALTIES
-GROUP BY `TITLE ID`, `AUTHOR ID`;
+(SELECT  t.title_id,
+	 ta.au_id,
+         t.advance,
+         T.price * s.qty * t.royalty / 100 * ta.royaltyper / 100 AS 'ROYALTY'
+
+FROM titles AS t 
+INNER JOIN titleauthor AS ta
+ON t.title_id = ta.title_id
+INNER JOIN sales AS s
+ON t.title_id = s.title_id) as royalties
+
+GROUP BY royalties.title_id, royalties.au_id, royalties.advance) as tr
+
+GROUP BY tr.au_id;
+
+-- Challenge 2 - Alternative Solution 
+
+-- Challenge 3
+
