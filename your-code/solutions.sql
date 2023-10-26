7325c2a7f1fc7f64a1922df74f6ab506cebd129e
Kohkitos <sanzyagain@gmail.com>
2023-10-26 10:53:23 +0200

    Everything completed but have to check ordr

--- your-code/solutions.sql
+++ your-code/solutions.sql
@@ -48,3 +48,30 @@ group by
 order by
 	'TOTAL' desc
 limit 3;
+
+-- New Table --
+
+create table most_profiting_authors
+(
+select
+	au.au_id as 'AUTHOR ID',
+	au_lname as 'LAST NAME',
+	au_fname as 'FIRST NAME',
+	SUM(tl.price * sl.qty * tl.royalty / 100 * tlau.royaltyper / 100) + SUM(tl.price * sl.qty * tl.advance) as 'TOTAL'
+from
+	authors as au
+left join
+    titleauthor as tlau on
+	au.au_id = tlau.au_id
+left join
+    titles as tl on
+	tlau.title_id = tl.title_id
+left join
+    sales as sl on
+	tl.title_id = sl.title_id
+group by
+	au.au_id,
+	tl.title_id
+order by
+	'TOTAL' desc
+limit 3);
